aec1004acfa52d5e9bd66537ddef55b745b53796
kclassix <63685521+kclassix@users.noreply.github.com>
2025-02-06 02:42:50 +0100

    Add files via upload

---
+++ functions/api.js
@@ -0,0 +1,22 @@
+import express from "express";
+import ServerlessHttp from "serverless-http";
+import cors from "cors";
+import * as bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors("*"));
+
+app.use(bodyParser.json());
+
+app.post("/.netlify/functions/api", async (req, res) => {
+
+  res.send("This IP address is not using a VPN. ");
+});
+
+const handler = ServerlessHttp(app);
+
+module.exports.handler = async (event, context) => {
+  const result = await handler(event, context);
+  return result;
+};
