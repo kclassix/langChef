d4fcb1f72adf047c99bc2b902ba5c27839e3b40b
kclassix <63685521+kclassix@users.noreply.github.com>
2025-02-06 03:14:38 +0100

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -10,8 +10,51 @@ app.use(cors("*"));
 app.use(bodyParser.json());
 
 app.post("/.netlify/functions/api", async (req, res) => {
-let request = req.body;
-  console.log(JSON.parse(request))
+let request = JSON.parse(request);
+  console.log(request)
+  async function translate(text, targetLanguage) {
+  console.log(text, targetLanguage);
+  let enText = text;
+  let toLang = targetLanguage;
+
+  let text2 = `f.req=[[["AVdN8","[\\"${enText}\\",\\"en\\",\\"${toLang}\\"]",null,"generic"]]]&`;
+  let encodedText = encodeURI(text2);
+  searchSrting = encodedText.replaceAll(",", "%2C");
+  try {
+    let response = await fetch(
+      "https://translate.google.com/_/TranslateWebserverUi/data/batchexecute",
+      {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          Host: "translate.google.com",
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+        body: searchSrting,
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    let result = await response.text();
+    let resultPro = result.replace(/\\/g, "");
+    let transTag = resultPro.slice(resultPro.indexOf(`"[[["`) + 5);
+    console.log(transTag.slice(0, transTag.indexOf(`","`)));
+    console.log(
+      transTag.slice(transTag.indexOf(`","`) + 3, transTag.indexOf(`"],["`))
+    );
+    return transTag.slice(
+      transTag.indexOf(`","`) + 3,
+      transTag.indexOf(`"],["`)
+    );
+  } catch (error) {
+    console.error("Translation error:", error);
+    return "Translation failed.";
+  }
+}
+
+translate(request.question, request.targetLanguage);
+
   res.send("This IP address is not using a VPN. ");
 });
 
