a323a41bff514aa88d55614aad6af2bc11930329
kclassix <63685521+kclassix@users.noreply.github.com>
2025-02-06 05:23:14 +0100

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -11,56 +11,54 @@ app.use(bodyParser.json());
 
 
 async function translate(text, targetLanguage) {
-  console.log(text, targetLanguage);
-  let enText = text;
-  let toLang = targetLanguage;
+    console.log(text, targetLanguage);
+    let enText = text;
+    let toLang = targetLanguage;
 
-  let text2 = `f.req=[[["AVdN8","[\\"${enText}\\",\\"en\\",\\"${toLang}\\"]",null,"generic"]]]&`;
-  let encodedText = encodeURI(text2);
-  searchSrting = encodedText.replaceAll(",", "%2C");
-  try {
-    let response = await fetch(
-      "https://translate.google.com/_/TranslateWebserverUi/data/batchexecute",
-      {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          Host: "translate.google.com",
-          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        },
-        body: searchSrting,
-      }
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+    let text2 = `f.req=[[["MkEWBc","[[\\"${enText}\\",\\"en\\",\\"${toLang}\\",1],[]]",null,"generic"]]]&`;
+    let encodedText = encodeURI(text2);
+    searchSrting = encodedText.replaceAll(",", "%2C");
+    try {
+        let response = await fetch(
+            "https://translate.google.com/_/TranslateWebserverUi/data/batchexecute",
+            {
+                method: "POST",
+                mode: "no-cors",
+                headers: {
+                    Host: "translate.google.com",
+                    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                },
+                body: searchSrting,
+            }
+        );
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let result = await request.text();
+        let resultPro = result.replace(/\\/g, '');
+        let transTag = resultPro.slice(resultPro.indexOf(`[[[null,null,null,null,null,[["`) + 31, resultPro.indexOf(`",null,null,null,null,null,"`));
+
+        return transTag
+
+    } catch (error) {
+        console.error("Translation error:", error);
+        return "Translation failed.";
     }
-    let result = await response.text();
-    // let resultPro = result.replace(/\\/g, "");
-    // let transTag = resultPro.slice(resultPro.indexOf(`"[[["`) + 5);
-    // console.log(transTag.slice(0, transTag.indexOf(`","`)));
-   
-    // return transTag.slice(transTag.indexOf(`","`) + 3, transTag.indexOf(`"],["`))
-    return result
-    
-  } catch (error) {
-    console.error("Translation error:", error);
-    return "Translation failed.";
-  }
 };
 
 app.post("/.netlify/functions/api", async (req, res) => {
-let request = JSON.parse(req.body);
-  console.log(request)
+    let request = JSON.parse(req.body);
+    console.log(request)
 
-  let translated = await translate(request.question, request.targetLanguage);
-console.log(translated);
+    let translated = await translate(request.question, request.targetLanguage);
+    console.log(translated);
 
-  res.send(translated);
+    res.send(translated);
 });
 
 const handler = ServerlessHttp(app);
 
 module.exports.handler = async (event, context) => {
-  const result = await handler(event, context);
-  return result;
+    const result = await handler(event, context);
+    return result;
 };
